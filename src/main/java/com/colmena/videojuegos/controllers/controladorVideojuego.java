4d07dc3f52c79fba42e5e7be8d6914264cfa048d
anto guarnieri <antosofiag@gmail.com>
2021-10-26 11:34:03 -0300

    creacion de pagina de detalle

--- src/main/java/com/colmena/videojuegos/controllers/controladorVideojuego.java
+++ src/main/java/com/colmena/videojuegos/controllers/controladorVideojuego.java
@@ -3,9 +3,11 @@ package com.colmena.videojuegos.controllers;
 import com.colmena.videojuegos.entidades.Videojuego;
 import com.colmena.videojuegos.services.ServicioVideojuego;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -21,6 +23,19 @@ public class controladorVideojuego {
             model.addAttribute("videojuegos", videojuegos);
 
             return "view/inicio";
+        }catch (Exception e){
+            model.addAttribute("error", e.getMessage());
+            return "error";
+        }
+    }
+
+    @GetMapping ("/detalle/{id}")
+    public String detalleVideojuego (Model model, @PathVariable("id")Long id){
+        try
+        {
+            Videojuego videojuego = this.svcVideojuego.findByIdAndActivo(id);
+            model.addAttribute("videojuego", videojuego);
+            return "view/detalle";
         }catch (Exception e){
             model.addAttribute("error", e.getMessage());
             return "error";
